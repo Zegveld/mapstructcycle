b6d923fff728e0f17b4e13afbfaa8286010f8f18
Cassius Vinicius de Magalhães <cassius@stf.jus.br>
2022-05-27 07:54:56 -0300

    Little adjustments

--- src/main/java/br/eti/cvm/mapstructcycle/controller/MotherController.java
+++ src/main/java/br/eti/cvm/mapstructcycle/controller/MotherController.java
@@ -1,13 +1,15 @@
 package br.eti.cvm.mapstructcycle.controller;
 
 import br.eti.cvm.mapstructcycle.dto.MotherDto;
-import br.eti.cvm.mapstructcycle.mapper.CycleAvoidingMappingContext;
 import br.eti.cvm.mapstructcycle.mapper.MotherMapper;
 import br.eti.cvm.mapstructcycle.repository.MotherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/mother")
@@ -20,8 +22,12 @@ public class MotherController {
     private MotherMapper motherMapper;
 
     @GetMapping()
-    public MotherDto showMother() {
-        return motherMapper.toDto(motherRepository.findById(1L).get(), new CycleAvoidingMappingContext());
+    public Set<MotherDto> listAllMothers() {
+        Set<MotherDto> mothers = new HashSet<>();
+
+        motherRepository.findAll().forEach(mother -> mothers.add(motherMapper.toDto(mother)));
+
+        return mothers;
     }
 
 }
